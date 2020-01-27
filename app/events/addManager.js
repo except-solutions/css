de0243b8f54252f406a5d3d9358d0cad11366a0c
Alexey Lebedev <lebedev.alexey@protonmail.com>
2020-01-27 12:12:56 +1000

    add manager creation feature

--- app/events/addManager.js
+++ app/events/addManager.js
@@ -1,6 +1,8 @@
 const css = require('../../css');
 
-css.onText(/\/add_manager (.+)/, async msg => {
-  const chatId = msg.chatId;
-  css.sendMessage(chatId, 'Received your message');
+const managerHelper = require('../helpers/addManagerHelper');
+
+css.onText(/\/i_am_manager/, async msg => {
+  const creationResult = await managerHelper.createMananger(msg.from);
+  css.sendMessage(msg.chat.id, creationResult.message);
 });
