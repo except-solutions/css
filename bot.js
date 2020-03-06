7ab129e3c5a58dea85414ecdb6ae1bae6455e173
Alexey Lebedev <lebedev.alexey@protonmail.com>
2020-03-06 16:06:15 +1000

    Add telegraf-sync, fix document sign in reply

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
-const Telegraf = require('telegraf');
+const Telegraf = require('telegraf-sync');
 const proxyAgent = require('./scripts/proxy');
+
 const cssbot = new Telegraf(process.env.TELEGRAM_TOKEN, {
   telegram: {
-    agent: proxyAgent()
-  }
+    agent: proxyAgent
+  },
 });
 
 module.exports = cssbot;
